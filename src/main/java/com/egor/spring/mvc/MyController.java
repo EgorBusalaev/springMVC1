f8ad3f33eb178827f1dc3fbf46ba6ce10df84bda
egorb <egorbusalaev@gmail.com>
2023-05-01 14:32:08 +0700

    Сделали запрос имени у сотрудника после ввода имени сделали отображение введенного имени

--- src/main/java/com/egor/spring/mvc/MyController.java
+++ src/main/java/com/egor/spring/mvc/MyController.java
@@ -5,8 +5,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller // контроллер тоже компонент, поэтому аанотацию @Component можно не писать
 public class MyController {
-@RequestMapping("/") // при указании этого адреса("/") будет выводиться наш view
+    @RequestMapping("/") // при указании этого адреса("/") будет выводиться наш view
     public String showFirstView() {
         return "first-View";
     }
+
+    @RequestMapping("/askDetails")
+    public String ascEmployeeDetails() {
+        return "ask-emp-details-view";
+    }
+@RequestMapping("showDetails")
+    public String showEmployeeDetails() {
+        return "show-emp-details-view";
+    }
+
 }
---
+++ src/main/webapp/WEB-INF/view/show-emp-details-view.jsp
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<html>
+
+<body>
+
+<h2> Dear employee WELCOME! </h2>
+<br>
+<br>
+<br>
+
+<h3> Your name: ${param.employeeName} </h3>
+
+</body>
+
+</html>
