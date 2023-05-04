35384fb4171efb0ad9347da39d753fa4d1423fa8
egorb <egorbusalaev@gmail.com>
2023-05-04 14:56:35 +0700

    @RequestMapping("/employee") для класса контроллер, работник будет указан перед всеми методами, но сейчас нужно будет прописать /employee/ в url

--- src/main/java/com/egor/spring/mvc/MyController.java
+++ src/main/java/com/egor/spring/mvc/MyController.java
@@ -9,37 +9,24 @@ import org.springframework.web.bind.annotation.RequestParam;
 import javax.servlet.http.HttpServletRequest;
 
 
-
 @Controller // контроллер тоже компонент, поэтому аанотацию @Component можно не писать
+@RequestMapping("/employee") // этот меэппинг называется контроллер мэппинг. /employee добавится перед всеми методам
 public class MyController {
     @RequestMapping("/") // при указании этого адреса("/") будет выводиться наш view
     public String showFirstView() {
         return "first-View";
     }
 
-    @RequestMapping("/askDetails")
+    @RequestMapping("/askDetails") // реквест метод над методом называется метод мэппинг
     public String ascEmployeeDetails() {
         return "ask-emp-details-view";
     }
 
-//    @RequestMapping("showDetails")
-//    public String showEmployeeDetails() {
-//        return "show-emp-details-view";
-//    }
-
-//    @RequestMapping("/showDetails")
-//    public String showEmployeeDetails(HttpServletRequest request, Model model) {
-//        String empName = request.getParameter("employeeName");
-//        empName = "Mr. " + empName;
-//        model.addAttribute("nameAttribute", empName);
-//        model.addAttribute("discription", "- Busalaev");
-//        return "show-emp-details-view";
-//    }
-@RequestMapping("/showDetails")
-public String showEmployeeDetails(@RequestParam("employeeName") String empName, Model model) {
+    @RequestMapping("/showDetails")
+    public String showEmployeeDetails(@RequestParam("employeeName") String empName, Model model) {
         empName = "Mr. " + empName;
-    model.addAttribute("nameAttribute", empName);
+        model.addAttribute("nameAttribute", empName);
 
-    return "show-emp-details-view";
-}
+        return "show-emp-details-view";
+    }
 }
